7f65e6e4b8f8698406227efcaa5d0eb3f5c5c6eb
Pruthul15 <your-email@example.com>
2026-04-30 19:41:29 -0400

    Add anomaly detection and quarantine pipeline

--- app/producer.py
+++ app/producer.py
@@ -1,35 +1,31 @@
 import time
 import json
+import random
 from kafka import KafkaProducer
-import yfinance as yf
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
 symbol = "AAPL"
+price = 271.35
 
 while True:
-    try:
-        stock = yf.Ticker(symbol)
-        data = stock.history(period="1d", interval="1m")
+    if random.random() < 0.2:
+        send_price = price * random.uniform(3, 6)
+        print("⚠️ Injecting anomaly...")
+    else:
+        send_price = price + random.uniform(-1, 1)
 
-        if not data.empty:
-            latest = data.tail(1).iloc[0]
+    message = {
+        "symbol": symbol,
+        "price": round(send_price, 2)
+    }
 
-            message = {
-                "symbol": symbol,
-                "price": float(latest["Close"]),
-            }
+    print("Sending:", message)
+    producer.send("market_data", message)
+    producer.flush()
 
-            print("Sending:", message)
-            producer.send("market_data", message)
-
-        # 🔥 IMPORTANT: slow down requests
-        time.sleep(90)
-
-    except Exception as e:
-        print("Error:", e)
-        print("Sleeping to avoid rate limit...")
-        time.sleep(120)
+    price = send_price
+    time.sleep(5)
